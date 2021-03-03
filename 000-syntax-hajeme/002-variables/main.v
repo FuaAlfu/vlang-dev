1db8c4ea6e65173e9b907a799a9e1e621a8a8b6e
FuaAlfu <55155669+FuaAlfu@users.noreply.github.com>
2021-03-03 21:40:24 +0300

    add

---
+++ 000-syntax-hajeme/002-variables/main.v
@@ -0,0 +1,12 @@
+fn main(){
+name := 'fua'
+age := 30  
+
+println(name)
+println(age)
+println('-----\n')
+println('age == ' + age.str())
+println('-----\n')
+println('my name is $name and my age is  $age')
+println("===end===\n")
+}
