f0de65e9be6b79e859f07ff1c1f1d0982231a055
FuaAlfu <55155669+FuaAlfu@users.noreply.github.com>
2021-03-04 17:54:37 +0300

    add

---
+++ 000-syntax-hajeme/003-moreVars/main.v
@@ -0,0 +1,11 @@
+fn main(){
+	/*
+	hexadecimal, binary or octal notation for integer literals
+	*/
+
+	h := 0x7B
+    b := 0b01111011
+    o := 0o173
+
+	println('hexadecimal: $h\n binary: $b\n octal notation: $o')
+}
