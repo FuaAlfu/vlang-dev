c63cbd3d3ebda23cb4bfafb78e61c8dd1238f5f3
FuaAlfu <55155669+FuaAlfu@users.noreply.github.com>
2021-03-05 20:39:06 +0300

    add

---
+++ 000-syntax-hajeme/004-conversion/main.v
@@ -0,0 +1,14 @@
+fn main() {
+	/*
+	strings can be easily converted to integers and vice versa
+	*/
+
+
+	s := '30'
+	println(s)  //here is our lovley string
+    sn := s.int()
+	println(sn) //our string converted to integer
+
+	n := 30
+    println(n.str()) //30 converted to string
+}
