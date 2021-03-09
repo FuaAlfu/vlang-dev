3ff4e2d9706ac228dda91cd68717fdf3d78c8278
FuaAlfu <55155669+FuaAlfu@users.noreply.github.com>
2021-03-09 19:33:06 +0300

    add

---
+++ 000-syntax-hajeme/014-required-fields/main.v
@@ -0,0 +1,29 @@
+struct Hoo {
+	a int [required]
+	b f32 [required]
+}
+
+fn main() {
+	/*
+	Required fields
+    we can mark a struct field with the [required] attribute,
+	to tell Vlang that  field must be initialized
+    when constructing that struct.
+	*/
+
+	//constructing :: instance
+	h := Hoo{
+		a: 300
+		b: 3.00
+	}
+	println('a is: $h.a and , b is: $h.b')
+	println('---')
+	//----------------------------------------------
+
+	//This example will not compile,
+   //since the field n isn't explicitly initialized:
+  /* _ = Hoo{}
+   println('_ = $_')
+   println('---')
+   */
+}
