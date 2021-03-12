47026feb02b050392e0e2a3dd088ee0415aceb14
FuaAlfu <55155669+FuaAlfu@users.noreply.github.com>
2021-03-12 04:44:56 +0300

    add

---
+++ 000-syntax-hajeme/017-constants/main.v
@@ -0,0 +1,16 @@
+const (
+	pi    = 3.14
+	golden_ratio = 1.6180
+)
+
+fn main() {
+	/*
+	Constants are declared with const keyword.
+	They can only be defined at the module level (outside of functions).
+	Constant values can never be changed.
+	we can also declare a single constant separately
+	*/
+	println(pi)
+    println(golden_ratio)
+}
+
