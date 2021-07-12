e5383cd5c2a0ea3300c3fb45d1bf496452afcb50
FuaAlfu <55155669+FuaAlfu@users.noreply.github.com>
2021-07-13 00:03:00 +0300

    add

---
+++ 000-syntax-hajeme/037-special-channel/main.v
@@ -0,0 +1,26 @@
+struct Abc {
+	x int
+}
+
+fn main(){
+	/*
+	For special purposes there are
+	some builtin properties and methods
+	*/
+	chano()
+}
+
+fn chano(){
+a := 2.13
+ch := chan f64{}
+res := ch.try_push(a) // try to perform `ch <- a`
+println(res)
+l := ch.len // number of elements in queue
+c := ch.cap // maximum queue length
+//is_closed := ch.closed // bool flag - has `ch` been closed
+println(l)
+println(c)
+//mut b := Abc{}
+//ch2 := chan Abc{}
+//res2 := ch2.try_pop(mut b) // try to perform `b = <-ch2`
+}
